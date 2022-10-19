fa7a5be71fe3caa12d3d9085f51ed1f61a863407
Arthur Cardoso <91923216+y-Arthur@users.noreply.github.com>
2022-10-19 20:52:02 -0300

    Add files via upload

---
+++ ddd-main/ddd_repository.py
@@ -0,0 +1,36 @@
+from ddd import Destino
+
+class ddd_repository:
+    destino_lista: Destino = []
+    
+
+    def __init__(self) -> None:
+        pass
+    
+    def checa_se_destino_existe(self,destino: Destino) -> bool:
+        for item in self.destino_lista: 
+            if (destino.ddd == item.ddd) or (destino.destino == item.destino):
+                return True
+        return False 
+    
+    def adicionar_destino(self,destino:Destino) -> bool:
+        if not self.checa_se_destino_existe(destino):
+            self.destino_lista.append(destino)
+            return True
+        return False
+
+    def obter_destino_pelo_ddd(self,ddd:int) -> str:
+        for  item in self.destino_lista:
+            if item.ddd == ddd:
+                return item.destino
+        return "DDD informado não cadastrado"
+    
+    def __str__(self) -> str:
+            formatText = "{0:<5} {1:<0}\n"
+            menu = formatText.format("DDD:", "Localidade:")       
+            
+            for item in self.destino_lista:
+                menu += formatText.format(item.destino, item.ddd)
+            
+            return menu
+ 
